0b9c1310a22e1b752ac3e398ea9be4c622806f64
Thibaut FONTAINE <tfontain@e2r11p5.42.fr>
2017-04-13 07:52:28 +0200

    norme fix

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 05:13:36 by tfontain          #+#    #+#             */
-/*   Updated: 2017/04/13 07:41:56 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/04/13 07:52:15 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,28 +22,25 @@ t_infos			init_blocks(t_infos info, int fl, char *name)
 	static int	count = 2;
 	t_block		*next;
 
-	if (fl == 0)
-		++count;
 	if (info.cur != NULL)
 		id = info.cur->id;
-	while (info.cur != NULL || info.cur->nxt->id != id || info.cur->id != info.cur->nxt->id)
+	while (info.cur != NULL || info.cur->nxt->id != id
+			|| info.cur->id != info.cur->nxt->id)
 		info.cur = info.cur->nxt;
 	if (info.cur != NULL)
 	{
 		next = info.cur->nxt;
-		info.cur->nxt = malloc(sizeof(t_block));
+		(info.cur->nxt = malloc(sizeof(t_block))) == NULL ? error() : 0;
 		info.cur = info.cur->nxt;
 	}
 	else
 	{
-		info.cur = malloc(sizeof(t_block));
+		(info.cur = malloc(sizeof(t_block))) == NULL ? error() : 0;
 		next = info.cur;
 	}
 	info.cur->nxt = next;
-	info.cur->id = fl == 0 ? count : fl;
+	info.cur->id = fl == 0 ? ++count : fl;
 	info.cur->name = name;
-	if (fl == 1 || fl == 2)
-		;
 	return (info);
 }
 
@@ -57,18 +54,15 @@ t_infos			parser(void)
 	info.ant = ft_atoi(line);
 	ft_strdel(&line);
 	info.cur = NULL;
-	if (info.ant <= 0)
-		error();
+	info.ant <= 0 ? error() : 0;
 	fl = 0;
 	while (get_next_line(0, &line) == 1)
 	{
-		if (line[0] == 'L' || line[0] == '#')
-			;
-		else if (ft_strcmp(line, "##start") == 0)
+		if (ft_strcmp(line, "##start") == 0)
 			fl = 1;
 		else if (ft_strcmp(line, "##end") == 0)
 			fl = 2;
-		else
+		else if (line[0] != 'L' && line[0] != '#')
 		{
 			info = init_blocks(info, fl,
 					ft_strsub(line, 0, ft_strchr(line, ' ') - line));
