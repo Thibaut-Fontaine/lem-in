ce3ed3721344da5dbf7b01e328c028f489915f29
Thibaut FONTAINE <tfontain@e2r11p5.42.fr>
2017-04-13 09:39:28 +0200

    printstruct add

--- main.c
+++ main.c
@@ -6,15 +6,34 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 04:36:40 by tfontain          #+#    #+#             */
-/*   Updated: 2017/04/13 05:55:37 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/04/13 09:34:59 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/includes/libft.h"
 #include "./lemin.h"
+#include <math.h> //
 
-int		main(void)
+int				printstruct(t_block *b) // to remove
 {
-	parser();
+	int			id;
+
+	id = b->id;
+	b = b->nxt;
+	while (b->id != id)
+	{
+		printf("id : %d | name : %s\n", b->id, b->name);
+		b = b->nxt;
+	}
+		printf("id : %d | name : %s\n", b->id, b->name);
+	return (1);
+}
+
+int				main(void)
+{
+	t_infos		info;
+
+	info = parser();
+	printstruct(info.cur);
 	return (0);
 }
