4687601416769550342d2f21668a0bf5158c8a7d
Thibaut FONTAINE <tfontain@e2r9p4.42.fr>
2017-04-15 16:09:35 +0200

    error file

--- error.c
+++ error.c
@@ -6,13 +6,13 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 06:49:18 by tfontain          #+#    #+#             */
-/*   Updated: 2017/04/14 16:31:32 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/04/15 15:54:30 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./lemin.h"
 
-int		error()
+int		error(void)
 {
 	ft_putstr("ERROR\n");
 	exit(-1);
