23064f9253c26829695ae2bc8bf1488c721f405c
Thibaut FONTAINE <tfontain@e2r2p20.42.fr>
2017-04-21 04:15:04 +0200

    infinite loop, it_is_finish() to fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 04:36:40 by tfontain          #+#    #+#             */
-/*   Updated: 2017/04/21 04:04:44 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/04/21 04:14:40 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,47 +36,13 @@ int				main(void)
 	t_infos		info;
 
 	info = parser();
-
-	//erase_block(find_block_id(info.cur, 3));
-	/*printf("----------------------\n");
-	init_block_weight(info.cur);*/
-	//printstruct(info.cur);
-	//printf("---------------------->\n");
-	//printf("---------------------->\n");
-	//
-	//
 	init_block_ant(info.cur, 0);
-	while (it_is_finish(info.cur) == 0)
+	while (it_is_finish(info.cur) == 0) // prlbm boucle infinie
 	{
-
 		follow_the_weights(info.cur);
 		ft_putchar('\n');
-		//printstruct(info.cur);
-		//follow_the_weights(info.cur);
-		//ft_putchar('\n');
-		//printstruct(info.cur);
 	}
-	//
-	//
-	//printf("----------------------\n");
-	//printstruct(info.cur);
-	//printf("----------------------\n");
-
-
-
-	//printf("\nnombre de fourmis : |%d|\n", info.ant);
-	//info.cur = find_block_id(info.cur, 2);
-	//if (info.cur->tubes)
-	//printf("le block : %s\n", info.cur->name);
-	/*t_tubes		*cur;
-
-	cur = info.cur->tubes;
-	while (cur != NULL)
-	{
-		printf("contient : %s\n", cur->content->name);
-		cur = cur->next;
-	}*/
-	//while (1)
-	//	;
+	while (1)
+		;
 	return (0);
 }
