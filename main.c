13bc50813614bd95a03d707f112cc45b20169f5a
Thibaut FONTAINE <tfontain@e2r12p11.42.fr>
2017-05-13 17:37:26 +0200

    fix loop

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 04:36:40 by tfontain          #+#    #+#             */
-/*   Updated: 2017/05/12 13:39:14 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/05/13 17:36:12 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int				main(void)
 	t_infos		info;
 
 	info = parser();
-	ft_sprint(PRINT_FILE);
 	ft_putchar('\n');
 	init_block_ant(info.cur, 0);
 	init_block_weight(info.cur);
+	if (find_block_id(info.cur, 1)->weight == 0)
+		error();
+	ft_sprint(PRINT_FILE);
 	while (it_is_finish(info.cur) == 0)
 	{
 		follow_the_weights(info.cur);
